f4613993dae2d4c45b441de78d353eb7f24a3719
duncdrum <d.paterson@me.com>
2021-04-10 03:32:04 +0200

    fix(views): get rid of lorem-ipsum
    tell the user what this is instead

--- src/main/polymer/src/my-view2.js
+++ src/main/polymer/src/my-view2.js
@@ -25,8 +25,9 @@ class MyView2 extends PolymerElement {
       <div class="card">
         <div class="circle">2</div>
         <h1>API</h1>
-        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.</p>
-        <p>Id nam odio natum malorum, tibique copiosae expetenda mel ea.Detracto suavitate repudiandae no eum. Id adhuc minim soluta nam.Id nam odio natum malorum, tibique copiosae expetenda mel ea.</p>
+        <p>There is a RESTful api for:</p>
+        <h2>Natural Language Processing</h2>
+        <h2>Named Entity Recognition</h2>
       </div>
     `;
   }
